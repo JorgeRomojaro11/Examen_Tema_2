2f41f8e5fc6efc14362afc01389ce4c916e3539e
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 12:16:22 +0100

    parte 4 C++

--- Manejo_Excepciones_C++.cpp
+++ Manejo_Excepciones_C++.cpp
@@ -1,3 +1,12 @@
-//
-// Created by Jorge Romojaro on 14/12/2023.
-//
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+// Excepción personalizada para errores en el formato de la fecha
+class FechaInvalidaException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: Formato de fecha no válido.";
+    }
+};
