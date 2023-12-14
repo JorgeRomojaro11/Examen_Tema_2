8e0f89c732898c7b75db181604d7729f223feaf3
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 11:59:37 +0100

    parte 2 C++

--- Gestion_Materias_C++.cpp
+++ Gestion_Materias_C++.cpp
@@ -12,3 +12,13 @@ struct Estudiante {
     float promedio;
     std::vector<Materia> materias;
 };
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+    std::cout << "Materias Inscritas:" << std::endl;
+    for (const Materia& materia : estudiante.materias) {
+        std::cout << " - " << materia.nombreMateria << ", Calificación: " << materia.calificacion << std::endl;
+    }
+}
