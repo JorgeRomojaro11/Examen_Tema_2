766c714769f55a0c2411029fe92e927bfa1ef7f0
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 12:03:13 +0100

    parte 2 C++

--- Gestion_Materias_C++.cpp
+++ Gestion_Materias_C++.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 struct Materia {
     std::string nombreMateria;
     float calificacion;
 };
-struct Estudiante {
-    std::string nombre;
-    int edad;
-    float promedio;
-    std::vector<Materia> materias;
-};
-void mostrarEstudiante(const Estudiante& estudiante) {
-    std::cout << "Nombre: " << estudiante.nombre << std::endl;
-    std::cout << "Edad: " << estudiante.edad << std::endl;
-    std::cout << "Promedio: " << estudiante.promedio << std::endl;
-
-    std::cout << "Materias Inscritas:" << std::endl;
-    for (const Materia& materia : estudiante.materias) {
-        std::cout << " - " << materia.nombreMateria << ", Calificación: " << materia.calificacion << std::endl;
-    }
-}
-void agregarMateria(Estudiante& estudiante, const std::string& nombreMateria, float calificacion) {
-    Materia nuevaMateria = {nombreMateria, calificacion};
-    estudiante.materias.push_back(nuevaMateria);
-}
