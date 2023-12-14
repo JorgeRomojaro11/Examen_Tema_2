1e7c000f649a5a6defb8d7c3f9b0ce8351c2d1a7
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 11:57:48 +0100

    parte 1 C

--- Estructura_Estudiante_C.cpp
+++ Estructura_Estudiante_C.cpp
@@ -5,3 +5,14 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+void mostrarEstudiante(const struct Estudiante* estudiante) {
+    printf("Nombre: %s\n", estudiante->nombre);
+    printf("Edad: %d\n", estudiante->edad);
+    printf("Promedio: %f\n", estudiante->promedio);
+}
+int main() {
+    struct Estudiante estudiante1 = {"Manolo", 24, 87.3};
+    mostrarEstudiante(&estudiante1);
+
+    return 0;
+}
