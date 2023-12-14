6efb45dd014f6479240397d4e0a55eaf5167e4bd
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 11:55:43 +0100

    parte 1

--- Estructura_Estudiante_C++.cpp
+++ Estructura_Estudiante_C++.cpp
@@ -6,3 +6,9 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+
