a653bf075fe58e51ae5f297d922c662d2e0ce90d
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-14 12:23:43 +0100

    parte 4 C

--- Manejo_Excepciones_C.cpp
+++ Manejo_Excepciones_C.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct Asistencia {
@@ -6,34 +7,3 @@ struct Asistencia {
     char materia[50];
     char estado[20];
 };
-enum Error {
-    SIN_ERROR,
-    FECHA_INVALIDA,
-    MATERIA_NO_REGISTRADA
-};
-struct Estudiante {
-    char nombre[50];
-    int edad;
-    float promedio;
-    struct Asistencia* asistencias;
-    int numAsistencias;
-};
-int registrarAsistencia(struct Estudiante* estudiante, const char* fecha, const char* materia, const char* estado) {
-    if (strlen(fecha) != 10 || fecha[4] != '-' || fecha[7] != '-') {
-        return FECHA_INVALIDA;
-    }
-
-    if (!materiaRegistrada(materia)) {
-        return MATERIA_NO_REGISTRADA;
-    }
-    estudiante->asistencias = (struct Asistencia*)realloc(estudiante->asistencias, (estudiante->numAsistencias + 1) * sizeof(struct Asistencia));
-    strncpy(estudiante->asistencias[estudiante->numAsistencias].fecha, fecha, sizeof(estudiante->asistencias[estudiante->numAsistencias].fecha));
-    strncpy(estudiante->asistencias[estudiante->numAsistencias].materia, materia, sizeof(estudiante->asistencias[estudiante->numAsistencias].materia));
-    strncpy(estudiante->asistencias[estudiante->numAsistencias].estado, estado, sizeof(estudiante->asistencias[estudiante->numAsistencias].estado));
-    estudiante->numAsistencias++;
-
-    return SIN_ERROR;
-}
-int materiaRegistrada(const char* materia) {
-    return (strcmp(materia, "Matemáticas") == 0 || strcmp(materia, "Historia") == 0);
-}
